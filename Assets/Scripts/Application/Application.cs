3c339869837ae654a5fb00581de83b9314abc3f1
Andrey Shatalov <asskunst@gmail.com>
2026-04-03 00:53:34 +0200

    feat(05-03): Application and EntitiesCatalog ECS World initialization with parallel Entity+GameObject creation
    - Application.cs: _useEcs flag, ECS singleton entities, CollisionBridge init, DeadEntityCleanupSystem callback
    - EntitiesCatalog.cs: ConnectEcs, ReleaseByGameObject, AddComponentObject<GameObjectRef> for all entity types
    - Merged AsteroidsBridge.asmdef into Asteroids assembly to resolve circular dependency
    - Added Unity.Mathematics and Unity.Collections references to Asteroids.asmdef

--- Assets/Scripts/Application/Application.cs
+++ Assets/Scripts/Application/Application.cs
@@ -1,4 +1,8 @@
+using System;
 using SelStrom.Asteroids.Configs;
+using SelStrom.Asteroids.ECS;
+using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace SelStrom.Asteroids
@@ -16,6 +20,9 @@ namespace SelStrom.Asteroids
         private EntitiesCatalog _catalog;
         private TitleScreen _titleScreen;
 
+        private bool _useEcs = true;
+        private CollisionBridge _collisionBridge;
+
         public void Connect(IApplicationComponent appComponent, GameData configs,
             Transform poolContainer, Transform gameContainer, GameScreen gameScreen, TitleScreen titleScreen)
         {
@@ -28,7 +35,7 @@ namespace SelStrom.Asteroids
 
             _gameObjectPool = new GameObjectPool();
             _gameObjectPool.Connect(poolContainer);
-            
+
             _catalog = new EntitiesCatalog();
         }
 
@@ -41,10 +48,59 @@ namespace SelStrom.Asteroids
             Debug.Log("Scene size: " + sceneWidth + " x " + sceneHeight);
 
             _model = new Model { GameArea = new Vector2(sceneWidth, sceneHeight) };
-            
+
             _catalog.Connect(_configs, new ModelFactory(_model), new ViewFactory(_gameObjectPool, _gameContainer));
-            
+
+            if (_useEcs)
+            {
+                var world = World.DefaultGameObjectInjectionWorld;
+                var em = world.EntityManager;
+
+                // Singleton entities
+                var gameAreaEntity = em.CreateEntity();
+                em.AddComponentData(gameAreaEntity, new GameAreaData
+                {
+                    Size = new float2(sceneWidth, sceneHeight)
+                });
+
+                var shipPosEntity = em.CreateEntity();
+                em.AddComponentData(shipPosEntity, new ShipPositionData());
+
+                var scoreEntity = em.CreateEntity();
+                em.AddComponentData(scoreEntity, new ScoreData { Value = 0 });
+
+                var collisionBufferEntity = em.CreateEntity();
+                em.AddBuffer<CollisionEventData>(collisionBufferEntity);
+
+                var gunEventEntity = em.CreateEntity();
+                em.AddBuffer<GunShootEvent>(gunEventEntity);
+
+                var laserEventEntity = em.CreateEntity();
+                em.AddBuffer<LaserShootEvent>(laserEventEntity);
+
+                // CollisionBridge
+                _collisionBridge = new CollisionBridge();
+                _collisionBridge.Initialize(em, collisionBufferEntity);
+
+                // DeadEntityCleanupSystem callback
+                var cleanupSystem = world.GetExistingSystemManaged<DeadEntityCleanupSystem>();
+                if (cleanupSystem != null)
+                {
+                    cleanupSystem.SetOnDeadEntityCallback(OnDeadEntity);
+                }
+
+                // EntitiesCatalog ECS
+                _catalog.ConnectEcs(em, _collisionBridge);
+            }
+
             _game = new Game(_catalog, _model, _configs, _playerInput, _gameScreen);
+
+            if (_useEcs)
+            {
+                var world = World.DefaultGameObjectInjectionWorld;
+                var em = world.EntityManager;
+                _game.ConnectEcs(_useEcs, _collisionBridge, em);
+            }
 
             _appComponent.OnUpdate += OnUpdate;
             _appComponent.OnPause += OnPause;
@@ -54,7 +110,49 @@ namespace SelStrom.Asteroids
 
             _titleScreen.Connect(() => {
                 _game.Start();
+
+                if (_useEcs)
+                {
+                    var world = World.DefaultGameObjectInjectionWorld;
+                    var bridge = world.GetExistingSystemManaged<ObservableBridgeSystem>();
+                    if (bridge != null)
+                    {
+                        bridge.SetShipViewModel(
+                            _catalog.GetShipViewModel(),
+                            _configs.Ship.MainSprite,
+                            _configs.Ship.ThrustSprite);
+                    }
+                }
             });
+        }
+
+        private void OnDeadEntity(GameObject go)
+        {
+            _collisionBridge.UnregisterMapping(go);
+
+            if (_catalog.TryFindModel<AsteroidModel>(go, out var asteroidModel))
+            {
+                _game.PlayEffect(_configs.VfxBlowPrefab, asteroidModel.Move.Position.Value);
+                var age = asteroidModel.Age - 1;
+                if (age > 0)
+                {
+                    var position = asteroidModel.Move.Position.Value;
+                    var speed = Math.Min(asteroidModel.Move.Speed.Value * 2, 10f);
+                    _catalog.CreateAsteroid(age, position, speed);
+                    _catalog.CreateAsteroid(age, position, speed);
+                }
+            }
+            else if (_catalog.TryFindModel<ShipModel>(go, out var shipModel))
+            {
+                _game.PlayEffect(_configs.VfxBlowPrefab, shipModel.Move.Position.Value);
+                _game.StopFromEcs();
+            }
+            else if (_catalog.TryFindModel<UfoBigModel>(go, out var ufoModel))
+            {
+                _game.PlayEffect(_configs.VfxBlowPrefab, ufoModel.Move.Position.Value);
+            }
+
+            _catalog.ReleaseByGameObject(go);
         }
 
         private void OnResume()
@@ -69,7 +167,16 @@ namespace SelStrom.Asteroids
 
         private void OnUpdate(float deltaTime)
         {
-            _model.Update(deltaTime);
+            if (_useEcs)
+            {
+                // ECS World updates automatically via Unity runtime
+                _model.ActionScheduler.Update(deltaTime);
+                _game.ProcessShootEvents();
+            }
+            else
+            {
+                _model.Update(deltaTime);
+            }
         }
 
         private void OnBack()
@@ -83,7 +190,7 @@ namespace SelStrom.Asteroids
         {
             _catalog.Dispose();
             _gameObjectPool.Dispose();
-            
+
             _catalog = null;
             _gameObjectPool = null;
             _appComponent = null;
@@ -101,6 +208,12 @@ namespace SelStrom.Asteroids
             _appComponent.OnPause -= OnPause;
             _appComponent.OnResume -= OnResume;
             _playerInput.OnBackAction -= OnBack;
+
+            if (_collisionBridge != null)
+            {
+                _collisionBridge.Clear();
+                _collisionBridge = null;
+            }
 
             Dispose();
         }
--- Assets/Scripts/Application/EntitiesCatalog.cs
+++ Assets/Scripts/Application/EntitiesCatalog.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using Model.Components;
 using SelStrom.Asteroids.Configs;
+using SelStrom.Asteroids.ECS;
 using Shtl.Mvvm;
+using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -15,10 +18,17 @@ namespace SelStrom.Asteroids
         private readonly Dictionary<IEntityView, IGameEntityModel> _visualToModel = new();
         private readonly Dictionary<GameObject, IGameEntityModel> _gameObjectToModel = new();
         private readonly Dictionary<IGameEntityModel, EventBindingContext> _modelToBindings = new();
+        private readonly Dictionary<GameObject, Entity> _gameObjectToEntity = new();
 
         private ModelFactory _modelFactory;
         private ViewFactory _viewFactory;
         private GameData _configs;
+
+        private EntityManager _entityManager;
+        private CollisionBridge _collisionBridge;
+        private bool _useEcs;
+
+        private ShipViewModel _lastShipViewModel;
 
         public ViewFactory ViewFactory => _viewFactory;
 
@@ -27,6 +37,18 @@ namespace SelStrom.Asteroids
             _modelFactory = modelFactory;
             _viewFactory = viewFactory;
             _configs = configs;
+        }
+
+        public void ConnectEcs(EntityManager entityManager, CollisionBridge collisionBridge)
+        {
+            _entityManager = entityManager;
+            _collisionBridge = collisionBridge;
+            _useEcs = true;
+        }
+
+        public ShipViewModel GetShipViewModel()
+        {
+            return _lastShipViewModel;
         }
 
         public bool TryFindModel<TModel>(GameObject gameObject, out TModel model)
@@ -40,6 +62,36 @@ namespace SelStrom.Asteroids
 
             model = default;
             return false;
+        }
+
+        public bool TryGetEntity(GameObject go, out Entity entity)
+        {
+            return _gameObjectToEntity.TryGetValue(go, out entity);
+        }
+
+        public void ReleaseByGameObject(GameObject go)
+        {
+            if (_gameObjectToModel.TryGetValue(go, out var model))
+            {
+                if (_modelToBindings.TryGetValue(model, out var bindings))
+                {
+                    bindings.CleanUp();
+                    _modelToBindings.Remove(model);
+                }
+
+                var view = _modelToVisual[model];
+                _modelToVisual.Remove(model);
+                _visualToModel.Remove(view);
+                _gameObjectToModel.Remove(go);
+
+                _viewFactory.Release(view);
+                _modelFactory.Release(model);
+            }
+
+            if (_gameObjectToEntity.TryGetValue(go, out _))
+            {
+                _gameObjectToEntity.Remove(go);
+            }
         }
 
         public ShipModel CreateShip(Action<Collision2D> onRegisterCollision)
@@ -56,6 +108,7 @@ namespace SelStrom.Asteroids
             model.Laser.ReloadRemaining.Value = _configs.Laser.LaserUpdateDurationSec;
 
             var viewModel = new ShipViewModel();
+            _lastShipViewModel = viewModel;
             var bindings = new EventBindingContext();
 
             bindings.From(model.Move.Position).To(viewModel.Position);
@@ -65,11 +118,40 @@ namespace SelStrom.Asteroids
                     sprite.Value = isThrust ? _configs.Ship.ThrustSprite : _configs.Ship.MainSprite);
 
             viewModel.OnCollision.Value = onRegisterCollision;
+
             bindings.InvokeAll();
 
             var view = _viewFactory.Get<ShipVisual>(_configs.Ship.Prefab);
             view.Connect(viewModel);
             AddToCatalog(model, view, bindings);
+
+            if (_useEcs)
+            {
+                var entity = EntityFactory.CreateShip(
+                    _entityManager,
+                    new float2(model.Move.Position.Value.x, model.Move.Position.Value.y),
+                    0f, // Ship starts stationary
+                    _configs.Ship.ThrustUnitsPerSecond,
+                    _configs.Ship.MaxSpeed,
+                    _configs.Ship.Gun.MaxShoots,
+                    _configs.Ship.Gun.ReloadDurationSec,
+                    _configs.Laser.LaserMaxShoots,
+                    _configs.Laser.LaserUpdateDurationSec
+                );
+                _entityManager.AddComponentObject(entity, new GameObjectRef
+                {
+                    Transform = view.transform,
+                    GameObject = view.gameObject
+                });
+                _collisionBridge.RegisterMapping(view.gameObject, entity);
+                _gameObjectToEntity[view.gameObject] = entity;
+
+                viewModel.OnCollision.Value = (col) =>
+                {
+                    _collisionBridge.ReportCollision(view.gameObject, col.gameObject);
+                };
+            }
+
             return model;
         }
 
@@ -83,12 +165,46 @@ namespace SelStrom.Asteroids
             var bindings = new EventBindingContext();
 
             bindings.From(model.Move.Position).To(viewModel.Position);
-            viewModel.OnCollision.Value = col => onRegisterCollision(model, col);
+
+            if (_useEcs)
+            {
+                // Collision will be set after view creation
+            }
+            else
+            {
+                viewModel.OnCollision.Value = col => onRegisterCollision(model, col);
+            }
+
             bindings.InvokeAll();
 
             var view = _viewFactory.Get<BulletVisual>(prefab);
             view.Connect(viewModel);
             AddToCatalog(model, view, bindings);
+
+            if (_useEcs)
+            {
+                var isPlayer = (prefab == _configs.Bullet.Prefab);
+                var entity = EntityFactory.CreateBullet(
+                    _entityManager,
+                    new float2(position.x, position.y),
+                    data.Speed,
+                    new float2(direction.x, direction.y),
+                    data.LifeTimeSeconds,
+                    isPlayer
+                );
+                _entityManager.AddComponentObject(entity, new GameObjectRef
+                {
+                    Transform = view.transform,
+                    GameObject = view.gameObject
+                });
+                _collisionBridge.RegisterMapping(view.gameObject, entity);
+                _gameObjectToEntity[view.gameObject] = entity;
+
+                viewModel.OnCollision.Value = col =>
+                {
+                    _collisionBridge.ReportCollision(view.gameObject, col.gameObject);
+                };
+            }
         }
 
         public void CreateAsteroid(int size, Vector2 position, float speed)
@@ -114,6 +230,25 @@ namespace SelStrom.Asteroids
             var view = _viewFactory.Get<AsteroidVisual>(data.Prefab);
             view.Connect(viewModel);
             AddToCatalog(model, view, bindings);
+
+            if (_useEcs)
+            {
+                var entity = EntityFactory.CreateAsteroid(
+                    _entityManager,
+                    new float2(model.Move.Position.Value.x, model.Move.Position.Value.y),
+                    model.Move.Speed.Value,
+                    new float2(model.Move.Direction.x, model.Move.Direction.y),
+                    size,
+                    data.Score
+                );
+                _entityManager.AddComponentObject(entity, new GameObjectRef
+                {
+                    Transform = view.transform,
+                    GameObject = view.gameObject
+                });
+                _collisionBridge.RegisterMapping(view.gameObject, entity);
+                _gameObjectToEntity[view.gameObject] = entity;
+            }
         }
 
         public void CreateBigUfo(ShipModel ship, Vector2 position, Vector2 direction,
@@ -131,12 +266,46 @@ namespace SelStrom.Asteroids
             var bindings = new EventBindingContext();
 
             bindings.From(model.Move.Position).To(viewModel.Position);
-            viewModel.OnCollision.Value = () => onRegisterCollision(model);
+
+            if (_useEcs)
+            {
+                // Collision callback set after view creation
+            }
+            else
+            {
+                viewModel.OnCollision.Value = () => onRegisterCollision(model);
+            }
+
             bindings.InvokeAll();
 
             var view = _viewFactory.Get<UfoVisual>(_configs.UfoBig.Prefab);
             view.Connect(viewModel);
             AddToCatalog(model, view, bindings);
+
+            if (_useEcs)
+            {
+                var entity = EntityFactory.CreateUfoBig(
+                    _entityManager,
+                    new float2(position.x, position.y),
+                    _configs.UfoBig.Speed,
+                    new float2(direction.x, direction.y),
+                    _configs.UfoBig.Gun.MaxShoots,
+                    _configs.UfoBig.Gun.ReloadDurationSec,
+                    3f, // shootToEvery -- hardcoded 1:1 with original
+                    _configs.UfoBig.Score
+                );
+                _entityManager.AddComponentObject(entity, new GameObjectRef
+                {
+                    Transform = view.transform,
+                    GameObject = view.gameObject
+                });
+                _collisionBridge.RegisterMapping(view.gameObject, entity);
+                _gameObjectToEntity[view.gameObject] = entity;
+
+                // UfoVisual collision doesn't pass col.gameObject, so we can't use CollisionBridge directly
+                // UfoVisual.OnCollisionEnter2D calls Action without params
+                // We leave the original callback for UFO collisions in non-ECS path
+            }
         }
 
         public void CreateUfo(ShipModel ship, Vector2 position, Vector2 direction,
@@ -156,12 +325,43 @@ namespace SelStrom.Asteroids
             var bindings = new EventBindingContext();
 
             bindings.From(model.Move.Position).To(viewModel.Position);
-            viewModel.OnCollision.Value = () => onRegisterCollision(model);
+
+            if (_useEcs)
+            {
+                // Collision callback set after view creation
+            }
+            else
+            {
+                viewModel.OnCollision.Value = () => onRegisterCollision(model);
+            }
+
             bindings.InvokeAll();
 
             var view = _viewFactory.Get<UfoVisual>(_configs.Ufo.Prefab);
             view.Connect(viewModel);
             AddToCatalog(model, view, bindings);
+
+            if (_useEcs)
+            {
+                var entity = EntityFactory.CreateUfo(
+                    _entityManager,
+                    new float2(position.x, position.y),
+                    _configs.Ufo.Speed,
+                    new float2(direction.x, direction.y),
+                    _configs.Ufo.Gun.MaxShoots,
+                    _configs.Ufo.Gun.ReloadDurationSec,
+                    3f, // shootToEvery -- hardcoded 1:1 with original
+                    3f, // moveToEvery -- hardcoded 1:1 with original
+                    _configs.Ufo.Score
+                );
+                _entityManager.AddComponentObject(entity, new GameObjectRef
+                {
+                    Transform = view.transform,
+                    GameObject = view.gameObject
+                });
+                _collisionBridge.RegisterMapping(view.gameObject, entity);
+                _gameObjectToEntity[view.gameObject] = entity;
+            }
         }
 
         private void AddToCatalog(IGameEntityModel model, IEntityView view, EventBindingContext bindings)
@@ -186,6 +386,14 @@ namespace SelStrom.Asteroids
             _visualToModel.Remove(view);
             _gameObjectToModel.Remove(view.gameObject);
 
+            if (_useEcs)
+            {
+                if (_gameObjectToEntity.TryGetValue(view.gameObject, out _))
+                {
+                    _gameObjectToEntity.Remove(view.gameObject);
+                }
+            }
+
             _viewFactory.Release(view);
             _modelFactory.Release(model);
         }
@@ -201,6 +409,12 @@ namespace SelStrom.Asteroids
             _modelToVisual.Clear();
             _visualToModel.Clear();
             _gameObjectToModel.Clear();
+
+            if (_useEcs)
+            {
+                _collisionBridge?.Clear();
+                _gameObjectToEntity.Clear();
+            }
         }
 
         public void Dispose()
