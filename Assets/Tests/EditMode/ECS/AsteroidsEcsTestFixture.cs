9a99c1bf4413714b3e703777ac5147adff46d45c
Andrey Shatalov <asskunst@gmail.com>
2026-04-02 23:47:37 +0200

    test(04-01): add ECS test fixture and component tests
    - AsteroidsEcsTestFixture with helper methods for creating all entity types
    - Helpers: CreateShipEntity, CreateAsteroidEntity, CreateBulletEntity, CreateUfoBigEntity, CreateUfoEntity
    - Singleton helpers: CreateGameAreaSingleton, CreateShipPositionSingleton, CreateScoreDataSingleton, CreateCollisionEventSingleton
    - 18 component tests covering all data, singleton, buffer, and tag components
    - Entity creation helpers add ScoreValue to Asteroid, Ufo, UfoBig entities

---
+++ Assets/Tests/EditMode/ECS/AsteroidsEcsTestFixture.cs
@@ -0,0 +1,184 @@
+using NUnit.Framework;
+using Unity.Entities;
+using Unity.Mathematics;
+using SelStrom.Asteroids.ECS;
+
+namespace SelStrom.Asteroids.Tests.EditMode.ECS
+{
+    public class AsteroidsEcsTestFixture
+    {
+        protected World World;
+        protected EntityManager m_Manager;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            World = new World("Test");
+            m_Manager = World.EntityManager;
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            if (World != null && World.IsCreated)
+            {
+                World.Dispose();
+            }
+
+            World = null;
+        }
+
+        protected Entity CreateShipEntity(float2 position = default, float speed = 0f)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new ShipTag());
+            m_Manager.AddComponentData(entity, new MoveData
+            {
+                Position = position,
+                Speed = speed
+            });
+            m_Manager.AddComponentData(entity, new RotateData());
+            m_Manager.AddComponentData(entity, new ThrustData());
+            m_Manager.AddComponentData(entity, new GunData());
+            m_Manager.AddComponentData(entity, new LaserData());
+            return entity;
+        }
+
+        protected Entity CreateAsteroidEntity(
+            float2 position, float speed, float2 direction, int age, int score = 100)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new AsteroidTag());
+            m_Manager.AddComponentData(entity, new MoveData
+            {
+                Position = position,
+                Speed = speed,
+                Direction = direction
+            });
+            m_Manager.AddComponentData(entity, new AgeData
+            {
+                Age = age
+            });
+            m_Manager.AddComponentData(entity, new ScoreValue
+            {
+                Score = score
+            });
+            return entity;
+        }
+
+        protected Entity CreateBulletEntity(
+            float2 position, float speed, float2 direction, float lifeTime, bool isPlayer)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new BulletTag());
+            m_Manager.AddComponentData(entity, new MoveData
+            {
+                Position = position,
+                Speed = speed,
+                Direction = direction
+            });
+            m_Manager.AddComponentData(entity, new LifeTimeData
+            {
+                TimeRemaining = lifeTime
+            });
+            if (isPlayer)
+            {
+                m_Manager.AddComponentData(entity, new PlayerBulletTag());
+            }
+            else
+            {
+                m_Manager.AddComponentData(entity, new EnemyBulletTag());
+            }
+            return entity;
+        }
+
+        protected Entity CreateUfoBigEntity(
+            float2 position, float speed, float2 direction, int score = 200)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new UfoBigTag());
+            m_Manager.AddComponentData(entity, new MoveData
+            {
+                Position = position,
+                Speed = speed,
+                Direction = direction
+            });
+            m_Manager.AddComponentData(entity, new GunData());
+            m_Manager.AddComponentData(entity, new ShootToData());
+            m_Manager.AddComponentData(entity, new ScoreValue
+            {
+                Score = score
+            });
+            return entity;
+        }
+
+        protected Entity CreateUfoEntity(
+            float2 position, float speed, float2 direction, int score = 500)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new UfoTag());
+            m_Manager.AddComponentData(entity, new MoveData
+            {
+                Position = position,
+                Speed = speed,
+                Direction = direction
+            });
+            m_Manager.AddComponentData(entity, new GunData());
+            m_Manager.AddComponentData(entity, new ShootToData());
+            m_Manager.AddComponentData(entity, new MoveToData());
+            m_Manager.AddComponentData(entity, new ScoreValue
+            {
+                Score = score
+            });
+            return entity;
+        }
+
+        protected Entity CreateGameAreaSingleton(float2 size)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new GameAreaData
+            {
+                Size = size
+            });
+            return entity;
+        }
+
+        protected Entity CreateShipPositionSingleton(
+            float2 position, float speed, float2 direction)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new ShipPositionData
+            {
+                Position = position,
+                Speed = speed,
+                Direction = direction
+            });
+            return entity;
+        }
+
+        protected Entity CreateScoreDataSingleton(int value = 0)
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new ScoreData
+            {
+                Value = value
+            });
+            return entity;
+        }
+
+        protected Entity CreateCollisionEventSingleton()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddBuffer<CollisionEventData>(entity);
+            return entity;
+        }
+
+        protected T CreateAndGetSystem<T>() where T : unmanaged, ISystem
+        {
+            World.CreateSystem<T>();
+            return World.Unmanaged.GetUnsafeSystemRef<T>(
+                World.GetExistingSystem<T>()
+            );
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/ECS/ComponentTests.cs
@@ -0,0 +1,284 @@
+using NUnit.Framework;
+using Unity.Entities;
+using Unity.Mathematics;
+using SelStrom.Asteroids.ECS;
+
+namespace SelStrom.Asteroids.Tests.EditMode.ECS
+{
+    [TestFixture]
+    public class ComponentTests : AsteroidsEcsTestFixture
+    {
+        [Test]
+        public void CreateEntity_WithMoveData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new MoveData());
+
+            var data = m_Manager.GetComponentData<MoveData>(entity);
+
+            Assert.AreEqual(float2.zero, data.Position);
+            Assert.AreEqual(0f, data.Speed);
+            Assert.AreEqual(float2.zero, data.Direction);
+        }
+
+        [Test]
+        public void CreateEntity_WithThrustData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new ThrustData());
+
+            var data = m_Manager.GetComponentData<ThrustData>(entity);
+
+            Assert.AreEqual(0f, ThrustData.MinSpeed);
+            Assert.AreEqual(0f, data.UnitsPerSecond);
+            Assert.AreEqual(0f, data.MaxSpeed);
+            Assert.IsFalse(data.IsActive);
+        }
+
+        [Test]
+        public void CreateEntity_WithRotateData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new RotateData());
+
+            var data = m_Manager.GetComponentData<RotateData>(entity);
+
+            Assert.AreEqual(90f, RotateData.DegreePerSecond);
+            Assert.AreEqual(0f, data.TargetDirection);
+            Assert.AreEqual(float2.zero, data.Rotation);
+        }
+
+        [Test]
+        public void CreateEntity_WithGunData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new GunData());
+
+            var data = m_Manager.GetComponentData<GunData>(entity);
+
+            Assert.AreEqual(0, data.CurrentShoots);
+            Assert.IsFalse(data.Shooting);
+            Assert.AreEqual(0f, data.ReloadRemaining);
+            Assert.AreEqual(0, data.MaxShoots);
+        }
+
+        [Test]
+        public void CreateEntity_WithLaserData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new LaserData());
+
+            var data = m_Manager.GetComponentData<LaserData>(entity);
+
+            Assert.AreEqual(0, data.CurrentShoots);
+            Assert.AreEqual(0f, data.ReloadRemaining);
+            Assert.AreEqual(0, data.MaxShoots);
+            Assert.IsFalse(data.Shooting);
+        }
+
+        [Test]
+        public void CreateEntity_WithShootToData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new ShootToData());
+
+            var data = m_Manager.GetComponentData<ShootToData>(entity);
+
+            Assert.AreEqual(0f, data.Every);
+            Assert.AreEqual(0f, data.ReadyRemaining);
+        }
+
+        [Test]
+        public void CreateEntity_WithMoveToData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new MoveToData());
+
+            var data = m_Manager.GetComponentData<MoveToData>(entity);
+
+            Assert.AreEqual(0f, data.Every);
+            Assert.AreEqual(0f, data.ReadyRemaining);
+        }
+
+        [Test]
+        public void CreateEntity_WithLifeTimeData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new LifeTimeData());
+
+            var data = m_Manager.GetComponentData<LifeTimeData>(entity);
+
+            Assert.AreEqual(0f, data.TimeRemaining);
+        }
+
+        [Test]
+        public void CreateEntity_WithAgeData_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new AgeData());
+
+            var data = m_Manager.GetComponentData<AgeData>(entity);
+
+            Assert.AreEqual(0, data.Age);
+        }
+
+        [Test]
+        public void CreateEntity_WithScoreValue_DefaultValuesCorrect()
+        {
+            var entity = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(entity, new ScoreValue());
+
+            var data = m_Manager.GetComponentData<ScoreValue>(entity);
+
+            Assert.AreEqual(0, data.Score);
+        }
+
+        [Test]
+        public void CreateEntity_WithTagComponents_HasComponentReturnsTrue()
+        {
+            var ship = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(ship, new ShipTag());
+
+            var asteroid = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(asteroid, new AsteroidTag());
+
+            var bullet = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(bullet, new BulletTag());
+
+            var ufo = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(ufo, new UfoTag());
+
+            var ufoBig = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(ufoBig, new UfoBigTag());
+
+            var playerBullet = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(playerBullet, new PlayerBulletTag());
+
+            var enemyBullet = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(enemyBullet, new EnemyBulletTag());
+
+            var dead = m_Manager.CreateEntity();
+            m_Manager.AddComponentData(dead, new DeadTag());
+
+            Assert.IsTrue(m_Manager.HasComponent<ShipTag>(ship));
+            Assert.IsTrue(m_Manager.HasComponent<AsteroidTag>(asteroid));
+            Assert.IsTrue(m_Manager.HasComponent<BulletTag>(bullet));
+            Assert.IsTrue(m_Manager.HasComponent<UfoTag>(ufo));
+            Assert.IsTrue(m_Manager.HasComponent<UfoBigTag>(ufoBig));
+            Assert.IsTrue(m_Manager.HasComponent<PlayerBulletTag>(playerBullet));
+            Assert.IsTrue(m_Manager.HasComponent<EnemyBulletTag>(enemyBullet));
+            Assert.IsTrue(m_Manager.HasComponent<DeadTag>(dead));
+        }
+
+        [Test]
+        public void CreateEntity_WithGameAreaData_SingletonReturnsCorrectSize()
+        {
+            var size = new float2(80f, 45f);
+            CreateGameAreaSingleton(size);
+
+            var query = m_Manager.CreateEntityQuery(typeof(GameAreaData));
+            var data = query.GetSingleton<GameAreaData>();
+
+            Assert.AreEqual(size.x, data.Size.x);
+            Assert.AreEqual(size.y, data.Size.y);
+        }
+
+        [Test]
+        public void CreateEntity_WithShipPositionData_SingletonReturnsCorrectValues()
+        {
+            var position = new float2(10f, 20f);
+            var speed = 5f;
+            var direction = new float2(1f, 0f);
+            CreateShipPositionSingleton(position, speed, direction);
+
+            var query = m_Manager.CreateEntityQuery(typeof(ShipPositionData));
+            var data = query.GetSingleton<ShipPositionData>();
+
+            Assert.AreEqual(position.x, data.Position.x);
+            Assert.AreEqual(position.y, data.Position.y);
+            Assert.AreEqual(speed, data.Speed);
+            Assert.AreEqual(direction.x, data.Direction.x);
+            Assert.AreEqual(direction.y, data.Direction.y);
+        }
+
+        [Test]
+        public void CreateEntity_WithScoreData_SingletonReturnsDefaultValue()
+        {
+            CreateScoreDataSingleton();
+
+            var query = m_Manager.CreateEntityQuery(typeof(ScoreData));
+            var data = query.GetSingleton<ScoreData>();
+
+            Assert.AreEqual(0, data.Value);
+        }
+
+        [Test]
+        public void CreateEntity_WithCollisionEventBuffer_CanAddElement()
+        {
+            var entity = CreateCollisionEventSingleton();
+
+            var buffer = m_Manager.GetBuffer<CollisionEventData>(entity);
+            var entityA = m_Manager.CreateEntity();
+            var entityB = m_Manager.CreateEntity();
+            buffer.Add(new CollisionEventData
+            {
+                EntityA = entityA,
+                EntityB = entityB
+            });
+
+            Assert.AreEqual(1, buffer.Length);
+            Assert.AreEqual(entityA, buffer[0].EntityA);
+            Assert.AreEqual(entityB, buffer[0].EntityB);
+        }
+
+        [Test]
+        public void CreateShipEntity_HasAllRequiredComponents()
+        {
+            var position = new float2(5f, 10f);
+            var entity = CreateShipEntity(position, 3f);
+
+            Assert.IsTrue(m_Manager.HasComponent<ShipTag>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<MoveData>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<RotateData>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<ThrustData>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<GunData>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<LaserData>(entity));
+
+            var moveData = m_Manager.GetComponentData<MoveData>(entity);
+            Assert.AreEqual(position.x, moveData.Position.x);
+            Assert.AreEqual(position.y, moveData.Position.y);
+            Assert.AreEqual(3f, moveData.Speed);
+        }
+
+        [Test]
+        public void CreateAsteroidEntity_HasScoreValue()
+        {
+            var entity = CreateAsteroidEntity(
+                new float2(1f, 2f), 5f, new float2(0f, 1f), 3, 150);
+
+            Assert.IsTrue(m_Manager.HasComponent<AsteroidTag>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<ScoreValue>(entity));
+
+            var score = m_Manager.GetComponentData<ScoreValue>(entity);
+            Assert.AreEqual(150, score.Score);
+
+            var age = m_Manager.GetComponentData<AgeData>(entity);
+            Assert.AreEqual(3, age.Age);
+        }
+
+        [Test]
+        public void CreateUfoEntity_HasScoreValueAndMoveToData()
+        {
+            var entity = CreateUfoEntity(
+                new float2(0f, 0f), 4f, new float2(1f, 0f), 500);
+
+            Assert.IsTrue(m_Manager.HasComponent<UfoTag>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<MoveToData>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<ScoreValue>(entity));
+            Assert.IsTrue(m_Manager.HasComponent<ShootToData>(entity));
+
+            var score = m_Manager.GetComponentData<ScoreValue>(entity);
+            Assert.AreEqual(500, score.Score);
+        }
+    }
+}
