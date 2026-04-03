b85b75bc4f6b11ba5bd63153dcad1388f21041a6
Andrey Shatalov <asskunst@gmail.com>
2026-04-03 16:58:29 +0200

    fix(06): ShootEventProcessorSystem — собрать events до итерации, затем обработать (structural changes вне Query loop)

--- Assets/Scripts/Bridge/ShootEventProcessorSystem.cs
+++ Assets/Scripts/Bridge/ShootEventProcessorSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using SelStrom.Asteroids.Configs;
 using SelStrom.Asteroids.ECS;
 using Unity.Collections;
@@ -44,8 +45,12 @@ namespace SelStrom.Asteroids
             ProcessLaserEvents();
         }
 
+        private readonly List<GunShootEvent> _pendingGunEvents = new();
+
         private void ProcessGunEvents()
         {
+            _pendingGunEvents.Clear();
+
             foreach (var buffer in SystemAPI.Query<DynamicBuffer<GunShootEvent>>())
             {
                 if (buffer.Length == 0)
@@ -53,24 +58,30 @@ namespace SelStrom.Asteroids
                     continue;
                 }
 
-                var events = buffer.ToNativeArray(Allocator.Temp);
-                buffer.Clear();
-
-                for (int i = 0; i < events.Length; i++)
+                for (int i = 0; i < buffer.Length; i++)
                 {
-                    var evt = events[i];
-                    var position = new Vector2(evt.Position.x, evt.Position.y);
-                    var direction = new Vector2(evt.Direction.x, evt.Direction.y);
-                    var prefab = evt.IsPlayer ? _configs.Bullet.Prefab : _configs.Bullet.EnemyPrefab;
-                    _catalog.CreateBullet(_configs.Bullet, prefab, position, direction);
+                    _pendingGunEvents.Add(buffer[i]);
                 }
 
-                events.Dispose();
+                buffer.Clear();
+            }
+
+            for (int i = 0; i < _pendingGunEvents.Count; i++)
+            {
+                var evt = _pendingGunEvents[i];
+                var position = new Vector2(evt.Position.x, evt.Position.y);
+                var direction = new Vector2(evt.Direction.x, evt.Direction.y);
+                var prefab = evt.IsPlayer ? _configs.Bullet.Prefab : _configs.Bullet.EnemyPrefab;
+                _catalog.CreateBullet(_configs.Bullet, prefab, position, direction);
             }
         }
 
+        private readonly List<LaserShootEvent> _pendingLaserEvents = new();
+
         private void ProcessLaserEvents()
         {
+            _pendingLaserEvents.Clear();
+
             foreach (var buffer in SystemAPI.Query<DynamicBuffer<LaserShootEvent>>())
             {
                 if (buffer.Length == 0)
@@ -78,59 +89,60 @@ namespace SelStrom.Asteroids
                     continue;
                 }
 
-                var events = buffer.ToNativeArray(Allocator.Temp);
-                buffer.Clear();
-
-                for (int i = 0; i < events.Length; i++)
+                for (int i = 0; i < buffer.Length; i++)
                 {
-                    var evt = events[i];
-                    var position = new Vector2(evt.Position.x, evt.Position.y);
-                    var direction = new Vector2(evt.Direction.x, evt.Direction.y);
+                    _pendingLaserEvents.Add(buffer[i]);
+                }
 
-                    var effect = _catalog.ViewFactory.Get<LineRenderer>(_configs.Laser.Prefab);
-                    effect.transform.position = position;
-                    var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                    effect.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-                    _actionScheduler.ScheduleAction(
-                        () => { _catalog.ViewFactory.Release(effect.gameObject); },
-                        _configs.Laser.BeamEffectLifetimeSec);
+                buffer.Clear();
+            }
 
-                    var hits = new RaycastHit2D[30];
-                    var size = Physics2D.RaycastNonAlloc(position, direction, hits,
-                        _gameArea.magnitude, LayerMask.GetMask("Asteroid", "Enemy"));
-                    if (size <= 0)
+            for (int i = 0; i < _pendingLaserEvents.Count; i++)
+            {
+                var evt = _pendingLaserEvents[i];
+                var position = new Vector2(evt.Position.x, evt.Position.y);
+                var direction = new Vector2(evt.Direction.x, evt.Direction.y);
+
+                var effect = _catalog.ViewFactory.Get<LineRenderer>(_configs.Laser.Prefab);
+                effect.transform.position = position;
+                var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                effect.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+                _actionScheduler.ScheduleAction(
+                    () => { _catalog.ViewFactory.Release(effect.gameObject); },
+                    _configs.Laser.BeamEffectLifetimeSec);
+
+                var hits = new RaycastHit2D[30];
+                var size = Physics2D.RaycastNonAlloc(position, direction, hits,
+                    _gameArea.magnitude, LayerMask.GetMask("Asteroid", "Enemy"));
+                if (size <= 0)
+                {
+                    continue;
+                }
+
+                for (var j = 0; j < size; j++)
+                {
+                    var hit = hits[j];
+                    var gameObject = hit.collider.gameObject;
+                    if (_catalog.TryGetEntity(gameObject, out var hitEntity))
                     {
-                        continue;
-                    }
-
-                    for (var j = 0; j < size; j++)
-                    {
-                        var hit = hits[j];
-                        var gameObject = hit.collider.gameObject;
-                        if (_catalog.TryGetEntity(gameObject, out var hitEntity))
+                        if (EntityManager.Exists(hitEntity) && !EntityManager.HasComponent<DeadTag>(hitEntity))
                         {
-                            if (EntityManager.Exists(hitEntity) && !EntityManager.HasComponent<DeadTag>(hitEntity))
+                            if (SystemAPI.HasSingleton<ScoreData>())
                             {
-                                // Начисляем очки
-                                if (SystemAPI.HasSingleton<ScoreData>())
+                                var scoreEntity = SystemAPI.GetSingletonEntity<ScoreData>();
+                                var scoreData = EntityManager.GetComponentData<ScoreData>(scoreEntity);
+                                if (EntityManager.HasComponent<ScoreValue>(hitEntity))
                                 {
-                                    var scoreEntity = SystemAPI.GetSingletonEntity<ScoreData>();
-                                    var scoreData = EntityManager.GetComponentData<ScoreData>(scoreEntity);
-                                    if (EntityManager.HasComponent<ScoreValue>(hitEntity))
-                                    {
-                                        scoreData.Value +=
-                                            EntityManager.GetComponentData<ScoreValue>(hitEntity).Score;
-                                        EntityManager.SetComponentData(scoreEntity, scoreData);
-                                    }
+                                    scoreData.Value +=
+                                        EntityManager.GetComponentData<ScoreValue>(hitEntity).Score;
+                                    EntityManager.SetComponentData(scoreEntity, scoreData);
                                 }
-
-                                EntityManager.AddComponent<DeadTag>(hitEntity);
                             }
+
+                            EntityManager.AddComponent<DeadTag>(hitEntity);
                         }
                     }
                 }
-
-                events.Dispose();
             }
         }
     }
